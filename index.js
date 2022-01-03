95a1b43f06aba100464ee28957886f3769fbfe7c
AG1234567890 <49893607+AG1234567890@users.noreply.github.com>
2022-01-03 15:07:58 -0800

    init commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// import DiscordJS, {Intents} from "discord.js"
-// import dotenv from "dotenv"
+const dotenv = require("dotenv");
 
-const { Client } = require("discord.js")
-const {Intents} = require("discord.js")
-const dotenv = require("dotenv")
 dotenv.config()
-//poggers championship :o
+
+
+
+const { Collection, Formatters, Client, Intents, MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const discord = require ("discord.js")
 
 const client = new Client({
     intents : [
@@ -20,22 +20,19 @@ client.once("ready", async () => {
     client.user.setActivity('your internet activity', { type: 'WATCHING' });
 })
 
-client.on("messageCreate", (message)=>{
-    if(message.content === "ping"){
-        message.reply({
-            content:"pong"
-        })
-    } else if (message.content.toLowerCase() === "taiwan" || message.content.toLowerCase() === "taiwanese"){
-        message.reply({
-            content:`
-            ATTENTION CITIZEN! 市民请注意!
+client.on("message", async (message) => {
 
-This is the Ministry of State Security. 您的浏览记录和活动引起了我们的注意 YOUR INTERNET ACTIVITY HAS ATTRACTED OUR ATTENTION. 同志們注意了 you have been found protesting in the discord server!!!!! 這是通知你，你必須認同我們將接管台灣 serious crime 以及世界其他地方 100 social credits have been deducted from your account 這對我們未來的所有下屬來說都是重要的機會 stop the protest immediately 立即加入我們的宣傳活動，提前獲得救贖 do not do this again! 不要再这样做! if you do not hesitate, more social credits ( -11115 social credits )will be subtracted from your profile, resulting in the subtraction of ration supplies. (由人民供应部重新分配 ccp) you'll also be sent into a re-education camp in the xinjiang uyghur autonomous zone.
+  if (message.content.includes("amogus")) {
+    message.channel.send("SUSSY AMOGUS WHEN THE IMPOSOTOR IS SUSSSY");
+    message.react('😄');
+  
+  } else if (message.content.includes("based")) {
+    message.channel.send(
+      `${message.author} Based? Based on what? Your mom? Be more specific`
+    );
+  } else if (message.content.includes("sad")) {
+    message.reply("cope harder")
+  }
+});
 
-为党争光! Glory to the CCP!
-            `
-        })
-    }
-})
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
